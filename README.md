be1e0d9955ffdd94298c85cd85da86b020eb4919
Watt Erikson <turtle@turtle-coin.com>
2018-12-03 19:55:46 -0800

    Hello catalyst

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# catalyst-blocks
+# egg-explorer
---
+++ config.js
@@ -0,0 +1,18 @@
+var api = 'https://catalyst.cncoin.cf/daemon';
+var donationAddress = "";
+var blockTargetInterval = 60;
+var coinUnits = 1000000;
+var symbol = 'CX';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+ "cx": [
+	["catalyst.cncoin.cf", "https://catalyst.cncoin.cf/api/"]
+]
+};
+
+var networkStat2 = {
+    "cx": [
+	[""]
+ ]
+};
